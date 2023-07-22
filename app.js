685a2349475b97555ed5eee0527ec1a4ebbd8338
Josh Fraser <joshua.david.fraser@gmail.com>
2023-07-22 11:54:17 -0700

    Serve HTML page

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import chalk from 'chalk';
 import debug from 'debug'
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// construct __dirname since it's not available in ES modules
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const logDebug = debug('app');
 
 const app = express();
-const logDebug = debug('app');
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, '/public/')));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
