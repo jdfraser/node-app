1bc35f3dd74fdebcd9f593716b05953d4738009b
Josh Fraser <joshua.david.fraser@gmail.com>
2023-07-22 11:31:03 -0700

    Add chalk and switch to ES modules

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require('express');
+import express from 'express';
+import chalk from 'chalk';
 
 const app = express();
-
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000');
+    console.log(`Listening on port ${chalk.green('3000')}`);
 })
