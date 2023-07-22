165b0ae01f4f240d55bd5a1426684958d1205118
Josh Fraser <joshua.david.fraser@gmail.com>
2023-07-22 11:42:01 -0700

    Add debug

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import chalk from 'chalk';
+import debug from 'debug'
 
 const app = express();
+const logDebug = debug('app');
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
 app.listen(3000, () => {
-    console.log(`Listening on port ${chalk.green('3000')}`);
+    logDebug(`Listening on port ${chalk.green('3000')}`);
 })
