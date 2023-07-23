1dd1600263c354d5ddfa35e5d8a0b5b05966ec1b
Josh Fraser <joshua.david.fraser@gmail.com>
2023-07-23 02:09:15 -0700

    Add chat box + send button

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField';
+import SendIcon from '@mui/icons-material/Send';
+import { ThemeProvider, createTheme, styled } from '@mui/material/styles';
 import './App.css';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark'
+  },
+});
+
+const SendButton = styled(Button)(() => ({
+  marginLeft:'1em'
+}))
 
 function App() {
   const [data, setData] = React.useState(null);
@@ -13,12 +26,20 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Button variant="contained">{!data ? 'Loading...' : data}</Button>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
+      <Container maxWidth={false}>
+        <TextField 
+            id="chat-input" 
+            variant="outlined"
+            label="Say something..."
+            multiline
+            maxRows={2}
+            size="small"
+        />
+        <SendButton variant="contained" endIcon={<SendIcon />}>Send</SendButton>
+      </Container>
+    </ThemeProvider>
   );
 }
 
