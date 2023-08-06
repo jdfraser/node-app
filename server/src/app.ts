d8eaf356dfe2bfdbcf883ebba32b43b2db748385
Josh Fraser <joshua.david.fraser@gmail.com>
2023-08-06 11:01:53 -0700

    Add optional postgres database connection

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,6 +6,7 @@ import path from 'path';
 import http from 'http';
 import { fileURLToPath } from 'url';
 import { Server } from 'socket.io';
+import pg from 'pg';
 import Message from './api/models/message.js';
 
 // construct __dirname since it's not available in ES modules
@@ -21,6 +22,26 @@ const app: Express = express();
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, '../../client/build')));
 app.use(express.json());
+
+const testDbConnection = async () : Promise<void> => {
+    const db = new pg.Client({
+        user: 'node_chat',
+        password: 'dev',
+        database: 'node_chat',
+    });
+
+    try {
+        logDebug('Connecting to database...');
+        await db.connect();
+        await db.query('SELECT $1::text as message', ['Test Query']);
+        logDebug(`${chalk.green('Success')}`);
+    } catch (err) {
+        logDebug(`${chalk.red('Failure')}`);
+        logDebug(`${chalk.yellow('WARNING')}: Unable to connect to database. Message history will be lost on server restart.`)
+    } finally {
+        db.end();
+    }
+}
 
 const httpServer = http.createServer(app);
 const io = new Server(httpServer);
@@ -48,6 +69,8 @@ app.get('*', (req: Request, res: Response) => {
     res.sendFile(path.resolve(__dirname, '../..client/build', 'index.html'))
 })
 
-httpServer.listen(PORT, () => {
-    logDebug(`Listening on port ${chalk.green(PORT)}`);
+httpServer.listen(PORT, async () => {
+    await testDbConnection();
+    
+    logDebug(`Listening on port ${chalk.blue(PORT)}`);
 })  
